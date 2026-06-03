b6099904206ce684685ce52fc1b3c2f52f48cd9a
Muhammad Mustafa Amir <mustafaamir398a@gmail.com>
2026-06-03 22:16:54 +0500

    Uncommented and formatted C++ code for sorting

--- Assignment-1 (Basics)/Question-1.cpp
+++ Assignment-1 (Basics)/Question-1.cpp
@@ -1,83 +1,83 @@
-//#include <iostream>
-//using namespace std;
-//int main()
-//{
-//    int a, b, c, d, e;
-//    cout << "Enter 5 numbers: ";
-//    cin >> a >> b >> c >> d >> e;
-//
-//    if (a > b)
-//    {
-//        int temp = a;
-//        a = b;
-//        b = temp;
-//    }
-//    if (b > c)
-//    {
-//        int temp = b;
-//        b = c;
-//        c = temp;
-//    }
-//    if (c > d)
-//    {
-//        int temp = c;
-//        c = d;
-//        d = temp;
-//    }
-//    if (d > e)
-//    {
-//        int temp = d;
-//        d = e;
-//        e = temp;
-//    }
-//
-//    if (a > b)
-//    {
-//        int temp = a;
-//        a = b;
-//        b = temp;
-//    }
-//    if (b > c)
-//    {
-//        int temp = b;
-//        b = c;
-//        c = temp;
-//    }
-//    if (c > d)
-//    {
-//        int temp = c;
-//        c = d;
-//        d = temp;
-//    }
-//
-//    if (a > b)
-//    {
-//        int temp = a;
-//        a = b;
-//        b = temp;
-//    }
-//    if (b > c)
-//    {
-//        int temp = b;
-//        b = c;
-//        c = temp;
-//    }
-//
-//    if (a > b)
-//    {
-//        int temp = a;
-//        a = b;
-//        b = temp;
-//    }
-//
-//    // ascending order
-//    cout << "In Ascending order: ";
-//    cout << a << " " << b << " " << c << " " << d << " " << e << endl;
-//
-//    // descending order
-//    cout << "In Descending order: ";
-//    cout << e << " " << d << " " << c << " " << b << " " << a << endl;
-//
-//    system("pause");
-//    return 0;
-//}
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c, d, e;
+	cout << "Enter 5 numbers: ";
+	cin >> a >> b >> c >> d >> e;
+
+	if (a > b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b > c)
+	{
+		int temp = b;
+		b = c;
+		c = temp;
+	}
+	if (c > d)
+	{
+		int temp = c;
+		c = d;
+		d = temp;
+	}
+	if (d > e)
+	{
+		int temp = d;
+		d = e;
+		e = temp;
+	}
+
+	if (a > b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b > c)
+	{
+		int temp = b;
+		b = c;
+		c = temp;
+	}
+	if (c > d)
+	{
+		int temp = c;
+		c = d;
+		d = temp;
+	}
+
+	if (a > b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b > c)
+	{
+		int temp = b;
+		b = c;
+		c = temp;
+	}
+
+	if (a > b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+
+	// ascending order
+	cout << "In Ascending order: ";
+	cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+
+	// descending order
+	cout << "In Descending order: ";
+	cout << e << " " << d << " " << c << " " << b << " " << a << endl;
+
+	system("pause");
+	return 0;
+}
