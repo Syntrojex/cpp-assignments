6377e8060c7247ad3a51951c01e79fb85c02357c
Muhammad Mustafa Amir <mustafaamir398a@gmail.com>
2026-06-04 13:22:10 +0500

    q8 added

--- Assignment-1 (Basics and Selections)/Question-8.cpp
+++ Assignment-1 (Basics and Selections)/Question-8.cpp
@@ -1,51 +1,51 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	char input;
-//	cout << "Enter a Character: ";
-//	cin >> input;
-//
-//	if (input >= '0' && input <= '9')
-//	{
-//		cout << "Number" << endl;
-//	}
-//
-//	else if (input >= 'A' && input <= 'Z')
-//	{
-//		cout << "Upper-case Alphabet" << endl;
-//	}
-//
-//	else if (input >= 'a' && input <= 'z')
-//	{
-//		cout << "Lower-case Alphabet" << endl;
-//	}
-//
-//	else if (input == ',' || input == '.' || input == '?' || input == '"' || input == '\'' || input == ';' || input == ':' || input == '`')
-//	{
-//		cout << "Punctuation Mark" << endl;
-//	}
-//
-//	else if (input == '(' || input == ')' || input == '[' || input == ']' || input == '{' || input == '}')
-//	{
-//		cout << "Brackets/Parenthesis" << endl;
-//	}
-//
-//	else if (input == char(32))
-//	{
-//		cout << "Space" << endl;
-//	}
-//
-//	else if (input == '_' || input == '-' || input == '+' || input == '*' || input == '&' || input == '^' || input == '%' || input == '$' || input == '#' || input == '@' || input == '!' || input == '~' || input == '<' || input == '>' || input == '/' || input == '\\')
-//	{
-//		cout << "Symbol" << endl;
-//	}
-//
-//	else
-//	{
-//		cout << "Special Character" << endl;
-//	}
-//
-//	system("pause");
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	char input;
+	cout << "Enter a Character: ";
+	cin >> input;
+
+	if (input >= '0' && input <= '9')
+	{
+		cout << "Number" << endl;
+	}
+
+	else if (input >= 'A' && input <= 'Z')
+	{
+		cout << "Upper-case Alphabet" << endl;
+	}
+
+	else if (input >= 'a' && input <= 'z')
+	{
+		cout << "Lower-case Alphabet" << endl;
+	}
+
+	else if (input == ',' || input == '.' || input == '?' || input == '"' || input == '\'' || input == ';' || input == ':' || input == '`')
+	{
+		cout << "Punctuation Mark" << endl;
+	}
+
+	else if (input == '(' || input == ')' || input == '[' || input == ']' || input == '{' || input == '}')
+	{
+		cout << "Brackets/Parenthesis" << endl;
+	}
+
+	else if (input == char(32))
+	{
+		cout << "Space" << endl;
+	}
+
+	else if (input == '_' || input == '-' || input == '+' || input == '*' || input == '&' || input == '^' || input == '%' || input == '$' || input == '#' || input == '@' || input == '!' || input == '~' || input == '<' || input == '>' || input == '/' || input == '\\')
+	{
+		cout << "Symbol" << endl;
+	}
+
+	else
+	{
+		cout << "Special Character" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
